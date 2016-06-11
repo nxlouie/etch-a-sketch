96106718a13fed50c5c68a9fd360ce51b46d2d0b
Nathan Louie <nxlouie@umich.edu>
2016-06-11 15:55:03 -0400

    finished minimum requirements of project

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 $(document).ready(function(){
+
+	createNewGrid(32,32);
+});
+
+function clearFunction(){
+	$('#container').empty();
+	var length = prompt("Length:");
+	var width  = prompt("Width:");
+
+	createNewGrid(length, width);
+}
+
+
+function createNewGrid(length, width){
 	var addRow = '<div class="row"></div>';
 	var addBox = '<div class="box"></div>';
 
-	for(var i = 0; i < 32; i++){
+	var boxDim = 960 / length;
+
+	if((960 / width) < boxDim){
+		boxDim = 960/width;
+	};
+
+
+
+	for(var i = 0; i < length; i++){
 		$('#container').append(addRow);
 	}
-	for(var j = 0; j < 32; j++){
+	for(var j = 0; j < width; j++){
 		$('.row').append(addBox);
 	}
 
+	$('.box').css('height',boxDim + "px");
+	$('.box').css('width',boxDim + "px");
 
 	$(".box").hover(function(){
-	$(this).css("background-color","black");
+		$(this).css("background-color","black");
 	});
-
-})
-
+}
